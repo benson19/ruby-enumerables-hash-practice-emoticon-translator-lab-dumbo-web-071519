337f4e85a04cab4af506c4360bda4e59942cd332
Matthew Guzman <tomaffew@me.com>
2019-07-09 06:21:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file_path)
   hash.each{|key, value| 
   new_hash['get_meaning'][value[1]] = key
   new_hash['get_emoticon'][value[0]] = value[1] }
-  return new_hash
+  new_hash
 end
 
 def get_japanese_emoticon
