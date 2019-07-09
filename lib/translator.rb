9a7376294f7533000ba33031824511e67c74f13d
Matthew Guzman <tomaffew@me.com>
2019-07-09 06:19:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,12 @@
 require "yaml"
 
 def load_library
-  
+  hash = YAML.load_file(file_path)
+  new_hash = {'get_meaning' => {}, 'get_emoticon' => {}}
+  hash.each{|key, value| 
+  new_hash['get_meaning'][value[1]] = key
+  new_hash['get_emoticon'][value[0]] = value[1] }
+  new_hash
 end
 
 def get_japanese_emoticon
